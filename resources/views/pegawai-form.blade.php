3daeb0dd53220fd83ffa9623957f9c64e2311565
zikadwialyaputri <zika24ti@mahasiswa.pcr.ac.id>
2025-10-06 19:52:54 +0700

    Latihan Passing Data dan Git

--- resources/views/pegawai-form.blade.php
+++ resources/views/pegawai-form.blade.php
@@ -13,37 +13,31 @@
 
         <form action="{{ route('pegawai.process') }}" method="POST">
             @csrf
-
             <div class="mb-3">
                 <label class="form-label">Nama Lengkap</label>
                 <input type="text" name="name" class="form-control" value="{{ old('name') }}">
                 @error('name') <small class="text-danger">{{ $message }}</small> @enderror
             </div>
-
             <div class="mb-3">
                 <label class="form-label">Tanggal Lahir</label>
                 <input type="date" name="birth_date" class="form-control" value="{{ old('birth_date') }}">
                 @error('birth_date') <small class="text-danger">{{ $message }}</small> @enderror
             </div>
-
             <div class="mb-3">
                 <label class="form-label">Tanggal Harus Wisuda</label>
                 <input type="date" name="tgl_harus_wisuda" class="form-control" value="{{ old('tgl_harus_wisuda') }}">
                 @error('tgl_harus_wisuda') <small class="text-danger">{{ $message }}</small> @enderror
             </div>
-
             <div class="mb-3">
                 <label class="form-label">Semester Saat Ini</label>
                 <input type="number" name="current_semester" class="form-control" min="1" max="8" value="{{ old('current_semester') }}">
                 @error('current_semester') <small class="text-danger">{{ $message }}</small> @enderror
             </div>
-
             <div class="mb-3">
                 <label class="form-label">Cita-cita / Tujuan Karier</label>
                 <input type="text" name="future_goal" class="form-control" value="{{ old('future_goal') }}">
                 @error('future_goal') <small class="text-danger">{{ $message }}</small> @enderror
             </div>
-
             <div class="mb-3">
                 <label class="form-label">Hobi (Pilih minimal 5)</label><br>
                 @php
@@ -58,7 +52,6 @@
                 @endforeach
                 @error('hobbies') <br><small class="text-danger">{{ $message }}</small> @enderror
             </div>
-
             <button type="submit" class="btn btn-primary w-100">Tampilkan Data</button>
         </form>
     </div>
