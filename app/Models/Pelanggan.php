69b7cd3ff9b07bb2b79500d231ddac61df79c9ee
zikadwialyaputri <zika24ti@mahasiswa.pcr.ac.id>
2025-11-20 09:38:46 +0700

    Filter Dan Search
    BPF P11

--- app/Models/Pelanggan.php
+++ app/Models/Pelanggan.php
@@ -1,14 +1,14 @@
 <?php
-
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 
 class Pelanggan extends Model
 {
-    protected $table = 'pelanggan';
+    protected $table      = 'pelanggan';
     protected $primaryKey = 'pelanggan_id';
-    protected $fillable = [
+    protected $fillable   = [
         'first_name',
         'last_name',
         'birthday',
@@ -16,4 +16,23 @@ class Pelanggan extends Model
         'email',
         'phone',
     ];
+    public function scopeFilter(Builder $query, $request, array $filterableColumns): Builder
+    {
+        foreach ($filterableColumns as $column) {
+            if ($request->filled($column)) {
+                $query->where($column, $request->input($column));
+            }
+        }
+        return $query;
+    }
+    public function scopeSearch($query, $request, array $columns)
+    {
+        if ($request->filled('search')) {
+            $query->where(function ($q) use ($request, $columns) {
+                foreach ($columns as $column) {
+                    $q->orWhere($column, 'LIKE', '%' . $request->search . '%');
+                }
+            });
+        }
+    }
 }
