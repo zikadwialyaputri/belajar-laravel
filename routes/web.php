c8349fbe8a547ca52c265fa00ac00407475bbbe8
zikadwialyaputri <zika24ti@mahasiswa.pcr.ac.id>
2025-11-27 09:34:14 +0700

    Add Profile Picture
    BPF P12

--- routes/web.php
+++ routes/web.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\PelangganController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -60,3 +61,5 @@ Route::get('/auth', function () {
 });
 Route::get('/auth', [AuthController::class, 'index']);
 Route::post('/auth/login', [AuthController::class, 'login']);
+
+Route::resource('profile', ProfileController::class);
